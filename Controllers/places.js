1baa787aafe18f38c15533ee800c16a241cf7b48
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-10-31 15:23:11 -0400

    Edit functionality added but needs work..sends 404 code

--- Controllers/places.js
+++ Controllers/places.js
@@ -25,6 +25,20 @@ router.get('/new', (req,res) => {
   res.render('places/new')
 })
 
+
+router.get('/:id', (req,res) =>{
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  } 
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
+})
+
 router.get('/:id/edit', (req,res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -38,17 +52,26 @@ router.get('/:id/edit', (req,res) => {
   }
 })
 
-
-router.get('/:id', (req,res) =>{
+router.put('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
-    res.render('error404')
-  } 
+      res.render('error404')
+  }
   else if (!places[id]) {
-    res.render('error404')
+      res.render('error404')
   }
   else {
-    res.render('places/show', { place: places[id], id })
+      if (!req.body.pic) {
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
   }
 })
 
@@ -65,6 +88,5 @@ router.delete('/:id', (req, res) => {
     res.redirect('/places')
   }
 })
-
 
 module.exports = router
