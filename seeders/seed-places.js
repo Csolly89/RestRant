f6f96b2ef4f786a1b5838989e60f4a27aea02657
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-11-04 13:21:56 -0400

    seed info added

---
+++ seeders/seed-places.js
@@ -0,0 +1,22 @@
+const db = require('./models')
+
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai_rest.jpeg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/cat_rest.avif'
+}])
+.then(() => {
+    console.log('Success')
+    process.exit()})
+.catch(error => {
+    console.log('Failure', err)
+    process.exit()
+})
