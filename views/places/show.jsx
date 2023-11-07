71a3c2b70ab4e01011e2da010dd00c2b6d9bc1fb
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-11-07 17:13:54 -0500

    validation,seeding data and helper methods finished

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,19 +5,36 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{ data.place.name }</h1>
-                <div>
-                    <h2>Rating</h2>
-                    <p>Not Rated</p>
-                </div>
-                <div>
-                    <h2>Description</h2>
-                    <p>N/a</p>
-                </div>
-                <div>
-                    <h2>Comments</h2>
-                    <p>No Comments yet!</p>
-                </div>
+            <div className="row">
+                <div className="col-sm-6">
+                    <h1>{ data.place.name }</h1>
+                    <img className="placesImg" src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                        Located in {data.place.city}, {data.place.state}
+                        </h3>
+                </div>        
+                <div className="col-sm-6">
+                    <h3>
+                    {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                    Serving {data.place.cuisines}
+                    </h4>
+        
+                    <div>
+                        <h2>Rating</h2>
+                        <p>Not Rated</p>
+                    </div>
+                    <div>
+                        <h2>Description</h2>
+                        <p>N/a</p>
+                    </div>
+                    <div>
+                        <h2>Comments</h2>
+                        <p>No Comments yet!</p>
+                    </div>
+                </div>    
+            </div>        
             </main>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
