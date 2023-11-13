4cc05d260d0372d43785e2a974578a30e3d070e5
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-11-13 16:26:17 -0500

    delete and edit routes updated for mongoose

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -2,12 +2,12 @@ const React = require('react')
 const Def = require('../default.jsx')
 
 function edit_form (data) {
-    console.log(data)
+    // console.log(data)
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                     <div className="row">
                     <div className="form-group col-sm-6">
                         <label htmlFor="name">Place Name</label>
@@ -31,6 +31,10 @@ function edit_form (data) {
                     <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
                         <input className="form-control" id="cuisines" name="cuisines" value={data.place.cuisines} required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="founded">Founded</label>
+                        <input className="form-control"  id="founded" name="founded" value={data.place.founded} />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Edit Place" />
                 </form>
